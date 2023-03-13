46253bc43c1d634a3429e8d0018571faafb3a06b
imsayem <sayem@live.com>
2023-03-13 23:34:53 +0600

    add index.html

---
+++ success.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Success</title>
+  </head>
+  <body>
+    <h1>Success!</h1>
+    <p>Your form has been submitted.</p>
+    <a href="view_users.php">View Users</a>
+  </body>
+</html>
